8c5f66f3aeedea16ec4aff4fd3d7026def161f04
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-06-08 13:28:52 -0700

    added binary search trees hash tables quick-sort and test folder

---
+++ binary-search-tree/binary-search-tree.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+//know search, insert, depthFirstTraversal, breadthFirstTraversal
+function TreeNode(value) {
+  this.data = value;
+  this.left = null;
+  this.right = null;
+  this.parent = null;
+};
+
+function BinarySearchTree() {
+
+}
