d5e732714b0d413ba5702830fd1abe3526f2bd16
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-07-14 20:59:43 -0700

    added reduce polyfill

--- methods/reduce.js
+++ methods/reduce.js
@@ -53,3 +53,54 @@ var allbooks = friends.reduce(function(prev,curr) {
   return[...prev, ...curr.books];
 }, ['Alphabet']);
 //allbooks = ['Alphabet', 'Bible', 'Harry Potter', 'War and Peace', 'Romeo and Juliet', 'The Lord of the Rings', 'The Shining']
+
+//polyfill per MDN
+if(!Array.prototype.reduce) {
+  Object.defineProperty(Array.prototype, 'reduce', {
+    value: function(callback /*, initialValue*/) {
+      if (this === null) {
+        throw new TypeError('Array.prototype.reduce ' + 'called on null or undefined' );
+      }
+      if (typeof callback !== 'function') {
+        throw new TypeError( callback + 'is not a function');
+      }
+      //1. let 0 be ? ToObject(this value).
+      var o = Object(this);
+
+      //2 Let len be ? ToLength(? Get(0, 'length')).
+      var len = o.length >>> 0;
+
+      //steps 3,4,5,6,7
+      var k = 0;
+      var value;
+
+      if (arguments.length >= 2) {
+        value = arguments[1];
+      } else {
+        while (k < len && !(k in o)) {
+          k++;
+        }
+        //3. if len is 0 and initialValue is not present, throw a TypeError exception
+        if (k >= len) {
+          throw new TypeError('Reduce of empty array' + 'with no initial value');
+        }
+        value = o[k++];
+      }
+      //8. Repeat while k < len
+      while (k < len) {
+        //a. let pk be ! ToString(k).
+        //b let kPresent be ? HasProperty(0, Pk).
+        //c if kPresent is true, then
+        //  i. Let kValue be ? Get(0, Pk).
+        //  ii. Let accumulator be ? Call(callbackfn, undefined, <<accumulator, kValue, k, 0 >>).
+        if (k in o) {
+          value = callback(value, o[k], k, o);
+        }
+        //d Increase k by 1
+        k++;
+      }
+      //9 return accumulator
+      return value;
+    }
+  });
+}
